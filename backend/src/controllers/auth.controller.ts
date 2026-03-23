e1eab81b41da745cea064076b346aff77663b6db
GabrielcomH <ghabrielcamilo98@gmail.com>
2026-03-23 20:57:51 -0300

    atualizações da autenticação

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,32 @@
+import {Request, Response} from 'express';
+import {AuthService} from '../services/auth.service':
+
+const authService = new AuthService();
+
+export class authController {
+    async login(req: Request, res: Response){
+        try {
+            const {name, password} = req.body;
+
+            if (!name || !password) {
+                return res.status(400).json({
+                    status:'error',
+                    message: 'Nome e senha são obrigatórios.'
+                });
+            }
+
+            const resultado = await authService.login(name, password);  
+
+            if (!resultado) {
+                return res.status(401).json({
+                    status: 'error',
+                    message: 'Credenciais inválidas.'
+                });
+            }
+
+            res.json({ status: 'ok', data:resultado });
+        }   catch (error) {
+            res.status(500).json({status:'error', message: 'Erro no login'});       
+        }
+    }
+}
---
+++ backend/src/services/auth.service.ts
@@ -0,0 +1,26 @@
+import {AuthRepositories} from '../repositories/auth.repository';
+import crypto from 'crypto';
+
+const usuario = await AuthRepository ()
+
+export class AuthService {
+    async login(name: string, password: string) {
+        const usuario = await authRepository.findByName(name);
+
+        if (!usuario) return null;
+
+        const senhaCorreta = usuario.password === password;
+        if (!senhaCorreta) return null;
+
+        const token = crypto.randomUUID();
+
+        await authRepository.salvarToken(usuario.id, token);
+
+        return {
+            id: usuario.id,
+            name: usuario.name,
+            token
+        };
+
+    }
+}
