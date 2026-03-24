c7d8b0eeababba5f840d249b91758af501bc3376
Murilo Lopes <rodrigues.murilo2006@gmail.com>
2026-03-23 22:47:37 -0300

    Adicionando módulo de reuniões

---
+++ backend/src/services/reuniao.service.ts
@@ -0,0 +1,21 @@
+import { ReuniaoRepository } from '../repositories/reuniao.repository';
+
+const reuniaoRepository = new ReuniaoRepository();
+
+export class ReuniaoService {
+  async listarTodas() {
+    return await reuniaoRepository.findAll();
+  }
+
+  async criar(dados: any) {
+    return await reuniaoRepository.create(dados);
+  }
+
+  async adicionarParticipante(idReuniao: number, idParticipante: number) {
+    return await reuniaoRepository.addParticipante(idReuniao, idParticipante);
+  }
+
+  async listarParticipantes(idReuniao: number) {
+    return await reuniaoRepository.findParticipantes(idReuniao);
+  }
+}
